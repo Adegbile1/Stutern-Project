8eb5a85ca5f50f599c786a5650f68e8f6fd8c677
Adegbile1 <adegbilefunmilayo@gmail.com>
2023-03-30 15:25:27 +0100

    my react project

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+
+import "./App.css";
+import Header from "./Header";
+import Main from "./Main";
+
+const App = () => {
+  return (
+   <div>
+    <Header />
+    <Main />
+   </div>
+  );
+}
+
+export default App;
