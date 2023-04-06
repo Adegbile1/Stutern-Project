9012a5c1adb8e901c9742a0cbac2c5aec56896f5
Adegbile1 <adegbilefunmilayo@gmail.com>
2023-04-06 14:53:09 +0100

    updating

--- src/components/Searchsection.jsx
+++ src/components/Searchsection.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import googlelogo from '../img/googlelogo.png'
+import lens from '../img/imagelens.jpg'
 
 const Searchsection = () => {
   return (
-    <div>This is the body</div>
+    <div>
+      <img src= {googlelogo} alt='google'></img>
+      <div>
+        <form>
+          <input type='text' placeholder='googleicon'></input>
+          <button type='reset'>
+          <img src= {lens} alt='googlelens' style={{height: "60px", borderRadius: "10px"}}></img>
+          <img src= {lens} alt='googlelens' style={{height: "60px", borderRadius: "10px"}}></img>
+          </button>
+        </form>
+      </div>
+    </div>
+    
   )
 }
 
